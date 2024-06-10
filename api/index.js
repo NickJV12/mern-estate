d32f2278df5ffa419d8c48664d9b6642b887bac3
NickJV12 <shambhavim1202@gmail.com>
2024-06-10 18:39:17 +0530

    create sign in API route

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+app.listen(3030, () => {
+    console.log('Server is running on port 3030!');
 });
 
 app.use("/api/user", userRouter);
